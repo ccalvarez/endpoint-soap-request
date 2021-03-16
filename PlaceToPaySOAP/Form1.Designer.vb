b7601ff960ac8d23c7ef2bff1649d6d8f14115e7
Carolina Cordero <ccalvarez@gmail.com>
2021-03-16 11:25:13 -0600

    Initial commit

---
+++ PlaceToPaySOAP/Form1.Designer.vb
@@ -0,0 +1,110 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.lstResponses = New System.Windows.Forms.ListBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.AutoSize = True
+        Me.Button1.Location = New System.Drawing.Point(20, 51)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(4)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(229, 27)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Send Request (.NET Fwk 4.7.2)"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'lstResponses
+        '
+        Me.lstResponses.FormattingEnabled = True
+        Me.lstResponses.HorizontalScrollbar = True
+        Me.lstResponses.ItemHeight = 16
+        Me.lstResponses.Location = New System.Drawing.Point(31, 191)
+        Me.lstResponses.Margin = New System.Windows.Forms.Padding(4)
+        Me.lstResponses.Name = "lstResponses"
+        Me.lstResponses.Size = New System.Drawing.Size(1687, 548)
+        Me.lstResponses.TabIndex = 1
+        '
+        'Button2
+        '
+        Me.Button2.AutoSize = True
+        Me.Button2.Location = New System.Drawing.Point(265, 51)
+        Me.Button2.Margin = New System.Windows.Forms.Padding(4)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(289, 28)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Send Request (.NET Fwk 2.0)"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Button1)
+        Me.GroupBox1.Controls.Add(Me.Button2)
+        Me.GroupBox1.Location = New System.Drawing.Point(31, 33)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(621, 126)
+        Me.GroupBox1.TabIndex = 3
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Sin encabezado SOAP"
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(1460, 84)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(199, 42)
+        Me.Button5.TabIndex = 5
+        Me.Button5.Text = "Limpiar resultados"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1751, 868)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.lstResponses)
+        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents lstResponses As ListBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Button5 As Button
+End Class
---
+++ PlaceToPaySOAP/Form1.vb
@@ -0,0 +1,126 @@
+﻿Imports System.Net
+Imports System.ServiceModel
+Imports System.ServiceModel.Channels
+Imports System.Xml
+
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim endpointConsultas As wsPlaceToPay.WebServiceResTransaccionesClient = New wsPlaceToPay.WebServiceResTransaccionesClient()
+
+        ' Credenciales PlaceToPay TEST:
+        endpointConsultas.ClientCredentials.UserName.UserName = "..."
+        endpointConsultas.ClientCredentials.UserName.Password = "..."
+
+        ' Credenciales PayGate:
+        'endpointConsultas.ClientCredentials.UserName.UserName = "..."
+        'endpointConsultas.ClientCredentials.UserName.Password = "..."
+
+
+
+        Dim request As wsPlaceToPay.transaccion = Nothing  'request de consulta de la orden al PlaceToPay
+        Dim response As wsPlaceToPay.resultadoRespuestaTransacciones = Nothing 'response de consulta de la orden al PlaceToPay
+
+
+        request = New wsPlaceToPay.transaccion()
+
+        request.x_invoice_num = "3345"
+        request.x_amount = "2000"
+        request.x_date = "20210308"
+
+        Try
+            'response = endpointConsultas.consultaTransacciones(request.x_invoice_num, request.x_amount, request.x_date)
+
+
+            response = endpointConsultas.consultaTransacciones("3345", "2000", "20210308")
+
+
+            If IsNothing(response.mensajes) Then  ' cuando la orden fue TRANSACCION DENEGADA o TRANSACCION EXITOSA, mensajes viene vacío
+
+                If Not IsNothing(response.respuestaTransacciones.x_response_reason_text) Then
+                    If response.respuestaTransacciones.x_response_reason_text <> String.Empty Then
+                        lstResponses.Items.Add(response.respuestaTransacciones.x_response_reason_text)  ' en caso de null igualar a " "
+                    Else
+                        lstResponses.Items.Add(" ")
+                    End If
+                Else
+                    lstResponses.Items.Add(" ")
+                End If
+
+            Else
+                For Each msg As wsPlaceToPay.mensaje In response.mensajes
+                    lstResponses.Items.Add("Endpoint de consultas ha reportado: tipoMensaje=" & msg.tipoMensaje.ToString() & ", descripcion=" & msg.descripcion & ", codigo=" & msg.codigo & ", key=" & msg.key & ".  Con los parámetros:  x_invoice_num=" & request.x_invoice_num & ", x_amount=" & request.x_amount & ", x_date=" & request.x_date)
+                Next
+
+            End If
+        Catch ex As Exception
+            lstResponses.Items.Add("Ocurrió un error: " & ex.Message & If(Not (IsNothing(ex.InnerException)), ex.InnerException.ToString(), ""))
+
+        End Try
+
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        Dim endpointConsultas As New com.placetopay.test.WebServiceResTransaccionesService()
+
+        ' Credenciales PayGate:
+        'Dim credentials As ICredentials = New NetworkCredential("...", "...")
+
+        ' Credenciales PlaceToPay:
+        Dim credentials As ICredentials = New NetworkCredential("...", "...")
+
+
+        endpointConsultas.Credentials = credentials
+
+
+
+
+        Dim request As com.placetopay.test.transaccion = Nothing  'request de consulta de la orden al PlaceToPay
+        Dim response As com.placetopay.test.resultadoRespuestaTransacciones = Nothing 'response de consulta de la orden al PlaceToPay
+
+
+
+        request = New com.placetopay.test.transaccion()
+
+        request.x_invoice_num = "3345"
+        request.x_amount = "2000"
+        request.x_date = "20210308"
+
+        Try
+            'response = endpointConsultas.consultaTransacciones(request.x_invoice_num, request.x_amount, request.x_date)
+            response = endpointConsultas.consultaTransacciones("3345", "2000", "20210308")
+
+
+            If IsNothing(response.mensajes) Then  ' cuando la orden fue TRANSACCION DENEGADA o TRANSACCION EXITOSA, mensajes viene vacío
+
+                If Not IsNothing(response.respuestaTransacciones.x_response_reason_text) Then
+                    If response.respuestaTransacciones.x_response_reason_text <> String.Empty Then
+                        lstResponses.Items.Add(response.respuestaTransacciones.x_response_reason_text)  ' en caso de null igualar a " "
+                    Else
+                        lstResponses.Items.Add(" ")
+                    End If
+                Else
+                    lstResponses.Items.Add(" ")
+                End If
+
+            Else
+                For Each msg As com.placetopay.test.mensaje In response.mensajes
+                    lstResponses.Items.Add("Endpoint de consultas ha reportado: tipoMensaje=" & msg.tipoMensaje.ToString() & ", descripcion=" & msg.descripcion & ", codigo=" & msg.codigo & ", key=" & msg.key & ".  Con los parámetros:  x_invoice_num=" & request.x_invoice_num & ", x_amount=" & request.x_amount & ", x_date=" & request.x_date)
+                Next
+
+            End If
+        Catch ex As Exception
+            lstResponses.Items.Add("Ocurrió un error: " & ex.Message & If(Not (IsNothing(ex.InnerException)), ex.InnerException.ToString(), ""))
+
+        End Try
+
+    End Sub
+
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        lstResponses.Items.Clear()
+    End Sub
+End Class
+
